09d6a9ae922125654c7b1e26708d9ba1a5375506
Eugênio Moreira <eugeniojava@outlook.com>
2021-04-01 01:00:12 -0200

    refactor(junit): change code design

--- tdd-e-java-testes-automatizados-com-junit/tdd/src/main/java/model/Performance.java
+++ tdd-e-java-testes-automatizados-com-junit/tdd/src/main/java/model/Performance.java
@@ -1,8 +1,27 @@
 package model;
 
+import java.math.BigDecimal;
+
 public enum Performance {
 
-    TO_BE_DESIRED,
-    GOOD,
-    GREAT;
+    TO_BE_DESIRED {
+        @Override
+        public BigDecimal readjustmentPercent() {
+            return new BigDecimal("0.03");
+        }
+    },
+    GOOD {
+        @Override
+        public BigDecimal readjustmentPercent() {
+            return new BigDecimal("0.15");
+        }
+    },
+    GREAT {
+        @Override
+        public BigDecimal readjustmentPercent() {
+            return new BigDecimal("0.20");
+        }
+    };
+
+    public abstract BigDecimal readjustmentPercent();
 }
--- tdd-e-java-testes-automatizados-com-junit/tdd/src/main/java/service/ReadjustmentService.java
+++ tdd-e-java-testes-automatizados-com-junit/tdd/src/main/java/service/ReadjustmentService.java
@@ -8,18 +8,10 @@ import java.math.BigDecimal;
 public class ReadjustmentService {
 
     public void getReadjustment(Employee employee, Performance performance) {
-        String percentage;
-
-        if (performance == Performance.TO_BE_DESIRED) {
-            percentage = "0.03";
-        } else if (performance == Performance.GOOD) {
-            percentage = "0.15";
-        } else {
-            percentage = "0.20";
-        }
         BigDecimal readjustment = employee
                 .getSalary()
-                .multiply(new BigDecimal(percentage));
+                .multiply(performance.readjustmentPercent());
+
         employee.readjustSalary(readjustment);
     }
 }

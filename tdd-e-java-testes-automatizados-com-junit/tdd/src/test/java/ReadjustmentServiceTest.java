c5eda29caeab23d55f6f9daebc795ddb5680433a
Eugênio Moreira <eugeniojava@outlook.com>
2021-04-02 12:42:40 -0200

    refactor(junit): add some annotations

--- tdd-e-java-testes-automatizados-com-junit/tdd/src/test/java/ReadjustmentServiceTest.java
+++ tdd-e-java-testes-automatizados-com-junit/tdd/src/test/java/ReadjustmentServiceTest.java
@@ -1,6 +1,6 @@
 import model.Employee;
 import model.Performance;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import service.ReadjustmentService;
 
 import java.math.BigDecimal;
@@ -10,14 +10,36 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ReadjustmentServiceTest {
 
-    @Test
-    void readjustmentShouldBe3PercentWhenPerformanceIsEqualToToBeDesired() {
-        ReadjustmentService readjustmentService = new ReadjustmentService();
-        Employee employee = new Employee(
+    private ReadjustmentService readjustmentService;
+    private Employee employee;
+
+    @BeforeEach
+    public void initialize() {
+        System.out.println("Initializing...");
+        readjustmentService = new ReadjustmentService();
+        employee = new Employee(
                 "John",
                 LocalDate.now(),
                 new BigDecimal("1000.00"));
+    }
 
+    @AfterEach
+    public void finish() {
+        System.out.println("Finishing...");
+    }
+
+    @BeforeAll
+    public static void beforeAll() {
+        System.out.println("Before all");
+    }
+
+    @AfterAll
+    public static void afterAll() {
+        System.out.println("After all");
+    }
+
+    @Test
+    void readjustmentShouldBe3PercentWhenPerformanceIsEqualToToBeDesired() {
         readjustmentService.getReadjustment(employee,
                 Performance.TO_BE_DESIRED);
 
@@ -26,29 +48,17 @@ public class ReadjustmentServiceTest {
 
     @Test
     void readjustmentShouldBe15PercentWhenPerformanceIsEqualToGood() {
-        ReadjustmentService readjustmentService = new ReadjustmentService();
-        Employee employee = new Employee(
-                "John",
-                LocalDate.now(),
-                new BigDecimal("5000.00"));
-
         readjustmentService.getReadjustment(employee,
                 Performance.GOOD);
 
-        assertEquals(new BigDecimal("5750.00"), employee.getSalary());
+        assertEquals(new BigDecimal("1150.00"), employee.getSalary());
     }
 
     @Test
     void readjustmentShouldBe20PercentWhenPerformanceIsEqualToGreat() {
-        ReadjustmentService readjustmentService = new ReadjustmentService();
-        Employee employee = new Employee(
-                "John",
-                LocalDate.now(),
-                new BigDecimal("8000.00"));
-
         readjustmentService.getReadjustment(employee,
                 Performance.GREAT);
 
-        assertEquals(new BigDecimal("9600.00"), employee.getSalary());
+        assertEquals(new BigDecimal("1200.00"), employee.getSalary());
     }
 }

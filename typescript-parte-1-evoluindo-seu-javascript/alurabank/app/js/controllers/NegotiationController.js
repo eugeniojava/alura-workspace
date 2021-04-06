8c3a14d08f74849f6cd4a2ef72b96c65add3b18b
Eugênio Moreira <eugeniojava@outlook.com>
2021-04-06 16:22:33 -0200

    feat(typescript): add jQuery

--- typescript-parte-1-evoluindo-seu-javascript/alurabank/app/js/controllers/NegotiationController.js
+++ typescript-parte-1-evoluindo-seu-javascript/alurabank/app/js/controllers/NegotiationController.js
@@ -3,14 +3,14 @@ class NegotiationController {
         this._negotiations = new Negotiations();
         this._negotiationsView = new NegotiationsView("#negotiationsView");
         this._messageView = new MessageView("#messageView");
-        this._dateInput = document.querySelector("#date");
-        this._quantityInput = document.querySelector("#quantity");
-        this._priceInput = document.querySelector("#price");
+        this._dateInput = $("#date");
+        this._quantityInput = $("#quantity");
+        this._priceInput = $("#price");
         this._negotiationsView.update(this._negotiations);
     }
     add(event) {
         event.preventDefault();
-        const negotiation = new Negotiation(new Date(this._dateInput.value.replace(/-/g, ",")), parseInt(this._quantityInput.value), parseFloat(this._priceInput.value));
+        const negotiation = new Negotiation(new Date(this._dateInput.val().replace(/-/g, ",")), parseInt(this._quantityInput.val()), parseFloat(this._priceInput.val()));
         this._negotiations.add(negotiation);
         this._negotiationsView.update(this._negotiations);
         this._messageView.update("Negotiation sucessfully added!");
--- typescript-parte-1-evoluindo-seu-javascript/alurabank/app/ts/controllers/NegotiationController.ts
+++ typescript-parte-1-evoluindo-seu-javascript/alurabank/app/ts/controllers/NegotiationController.ts
@@ -1,15 +1,15 @@
 class NegotiationController {
-  private _dateInput: HTMLInputElement;
-  private _quantityInput: HTMLInputElement;
-  private _priceInput: HTMLInputElement;
+  private _dateInput: JQuery;
+  private _quantityInput: JQuery;
+  private _priceInput: JQuery;
   private _negotiations = new Negotiations();
   private _negotiationsView = new NegotiationsView("#negotiationsView");
   private _messageView = new MessageView("#messageView");
 
   constructor() {
-    this._dateInput = <HTMLInputElement>document.querySelector("#date");
-    this._quantityInput = <HTMLInputElement>document.querySelector("#quantity");
-    this._priceInput = <HTMLInputElement>document.querySelector("#price");
+    this._dateInput = $("#date");
+    this._quantityInput = $("#quantity");
+    this._priceInput = $("#price");
     this._negotiationsView.update(this._negotiations);
   }
 
@@ -17,9 +17,9 @@ class NegotiationController {
     event.preventDefault();
 
     const negotiation = new Negotiation(
-      new Date(this._dateInput.value.replace(/-/g, ",")),
-      parseInt(this._quantityInput.value),
-      parseFloat(this._priceInput.value)
+      new Date(this._dateInput.val().replace(/-/g, ",")),
+      parseInt(this._quantityInput.val()),
+      parseFloat(this._priceInput.val())
     );
 
     this._negotiations.add(negotiation);

7653b3137be060d732fb9affa53c7c264df2531d
Eugênio Moreira <eugeniojava@outlook.com>
2021-04-06 14:50:33 -0200

    refactor(typescript): use generic types

--- typescript-parte-1-evoluindo-seu-javascript/alurabank/app/js/views/NegotiationsView.js
+++ typescript-parte-1-evoluindo-seu-javascript/alurabank/app/js/views/NegotiationsView.js
@@ -1,7 +1,4 @@
 class NegotiationsView extends View {
-    update(model) {
-        this._element.innerHTML = this.template(model);
-    }
     template(model) {
         return `
     <table class="table table-hover table-bordered">

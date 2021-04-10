a7f1812355343f061382efb2cd4af1ec211ef8e3
Eugênio Moreira <eugeniojava@outlook.com>
2021-04-10 20:52:24 -0300

    refactor(typescrit): use readonly property

--- typescript-parte-2-mais-tecnicas-e-boas-praticas/alurabank/app/js/models/Negotiation.js
+++ typescript-parte-2-mais-tecnicas-e-boas-praticas/alurabank/app/js/models/Negotiation.js
@@ -6,22 +6,13 @@ System.register([], function (exports_1, context_1) {
         setters: [],
         execute: function () {
             Negotiation = class Negotiation {
-                constructor(_date, _quantity, _price) {
-                    this._date = _date;
-                    this._quantity = _quantity;
-                    this._price = _price;
-                }
-                get date() {
-                    return this._date;
-                }
-                get quantity() {
-                    return this._quantity;
-                }
-                get price() {
-                    return this._price;
+                constructor(date, quantity, price) {
+                    this.date = date;
+                    this.quantity = quantity;
+                    this.price = price;
                 }
                 get volume() {
-                    return this._quantity * this._price;
+                    return this.quantity * this.price;
                 }
             };
             exports_1("Negotiation", Negotiation);
--- typescript-parte-2-mais-tecnicas-e-boas-praticas/alurabank/app/ts/models/Negotiation.ts
+++ typescript-parte-2-mais-tecnicas-e-boas-praticas/alurabank/app/ts/models/Negotiation.ts
@@ -1,23 +1,11 @@
 export class Negotiation {
   constructor(
-    private _date: Date,
-    private _quantity: number,
-    private _price: number
+    readonly date: Date,
+    readonly quantity: number,
+    readonly price: number
   ) {}
 
-  get date() {
-    return this._date;
-  }
-
-  get quantity() {
-    return this._quantity;
-  }
-
-  get price() {
-    return this._price;
-  }
-
   get volume() {
-    return this._quantity * this._price;
+    return this.quantity * this.price;
   }
 }

b87bbb549d85b5eed450203b29bb796604e6c9b2
Eugênio Moreira <eugeniojava@outlook.com>
2021-04-25 23:13:36 -0300

    chore(webpack): change bootstrap to npm

--- webpack-manipulando-modulos-na-sua-webapp/webpack/client/app-src/ui/views/NegociacoesView.js
+++ webpack-manipulando-modulos-na-sua-webapp/webpack/client/app-src/ui/views/NegociacoesView.js
@@ -1,41 +1,44 @@
-import { View } from './View.js';
-import { DateConverter } from '../converters/DateConverter.js';
+import { View } from "./View.js";
+import { DateConverter } from "../converters/DateConverter.js";
 
 export class NegociacoesView extends View {
-
-    template(model) {
-
-        return `
+  template(model) {
+    return `
         <table class="table table-hover table-bordered">
             <thead>
                 <tr>
-                    <th>DATA</th>
-                    <th>QUANTIDADE</th>
-                    <th>VALOR</th>
+                    <th>DATE</th>
+                    <th>QUANTITY</th>
+                    <th>PRICE</th>
                     <th>VOLUME</th>
                 </tr>
             </thead>
-            
+
             <tbody>
-                ${model.paraArray().map(negociacao =>
-                `
+                ${model
+                  .paraArray()
+                  .map(
+                    (negociacao) =>
+                      `
                     <tr>
                         <td>${DateConverter.paraTexto(negociacao.data)}</td>
                         <td>${negociacao.quantidade}</td>
                         <td>${negociacao.valor}</td>
                         <td>${negociacao.volume}</td>
-                    </tr>                        
-                `).join('')}
+                    </tr>
+                `
+                  )
+                  .join("")}
             </tbody>
-            
+
             <tfoot>
                 <tr>
                     <td colspan="3"></td>
-                    <td>${model.volumeTotal}</td>            
+                    <td>${model.volumeTotal}</td>
                 </tr>
             </tfoot>
-            
-        </table>               
-        `
-    }
+
+        </table>
+        `;
+  }
 }

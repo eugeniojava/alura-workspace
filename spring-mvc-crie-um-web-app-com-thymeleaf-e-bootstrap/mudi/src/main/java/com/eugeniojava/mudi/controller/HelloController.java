93697bf786583a0160c5f449bbdb922a8d1ddc40
Eugênio Moreira <eugeniojava@outlook.com>
2021-04-27 00:27:20 -0300

    feat(spring): add first controller

---
+++ spring-mvc-crie-um-web-app-com-thymeleaf-e-bootstrap/mudi/src/main/java/com/eugeniojava/mudi/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.eugeniojava.mudi.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello(Model model) {
+        model.addAttribute("name", "world");
+
+        return "hello";
+    }
+}

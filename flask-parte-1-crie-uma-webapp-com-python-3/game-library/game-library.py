afa47ed115b607f7901d91b94493ad85981d172c
Eugênio Moreira <eugeniojava@outlook.com>
2021-06-07 01:52:04 -0300

    feat(flask): add dynamic content

--- flask-parte-1-crie-uma-webapp-com-python-3/game-library/game-library.py
+++ flask-parte-1-crie-uma-webapp-com-python-3/game-library/game-library.py
@@ -3,9 +3,19 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
+class Game:
+    def __init__(self, name, category, console):
+        self.name = name
+        self.category = category
+        self.console = console
+
+
 @app.route('/hello')
 def hello():
-    return render_template('list.html')
+    game1 = Game('League of Legends', 'MOBA', 'PC')
+    game2 = Game('Counter-Strike: Global Offensive', 'FPS', 'PC')
+    listOfGames = [game1, game2]
+    return render_template('list.html', title='Game Library', games=listOfGames)
 
 
 app.run(host='0.0.0.0', port=8080)

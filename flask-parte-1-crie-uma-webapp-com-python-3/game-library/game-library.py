64eb10f1fd2d742852499ee48904f3d4f6420809
Eugênio Moreira <eugeniojava@outlook.com>
2021-06-07 21:49:27 -0300

    feat(flask): add a form and use HTTP POST method

--- flask-parte-1-crie-uma-webapp-com-python-3/game-library/game-library.py
+++ flask-parte-1-crie-uma-webapp-com-python-3/game-library/game-library.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,12 +10,29 @@ class Game:
         self.console = console
 
 
-@app.route('/hello')
-def hello():
-    game1 = Game('League of Legends', 'MOBA', 'PC')
-    game2 = Game('Counter-Strike: Global Offensive', 'FPS', 'PC')
-    listOfGames = [game1, game2]
+game1 = Game('League of Legends', 'MOBA', 'PC')
+game2 = Game('Counter-Strike: Global Offensive', 'FPS', 'PC')
+listOfGames = [game1, game2]
+
+
+@app.route('/')
+def index():
     return render_template('list.html', title='Game Library', games=listOfGames)
 
 
-app.run(host='0.0.0.0', port=8080)
+@app.route('/new')
+def new():
+    return render_template('new.html', title='New game')
+
+
+@app.route('/create', methods=['POST'])
+def create():
+    name = request.form['name']
+    category = request.form['category']
+    console = request.form['console']
+    game = Game(name, category, console)
+    listOfGames.append(game)
+    return render_template('list.html', title='Game Library', games=listOfGames)
+
+
+app.run(host='0.0.0.0', port=8080, debug=True)

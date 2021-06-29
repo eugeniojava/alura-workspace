bb782c14716a3bfaa3d382976a231b66b79d7896
Eugênio Moreira <eugeniojava@outlook.com>
2021-06-29 18:34:51 -0300

    feat(spring): add repository and insert data

--- spring-data-jpa-repositorios-consultas-projecoes-e-specifications/spring-data/src/main/java/com/eugeniojava/springdata/SpringDataApplication.java
+++ spring-data-jpa-repositorios-consultas-projecoes-e-specifications/spring-data/src/main/java/com/eugeniojava/springdata/SpringDataApplication.java
@@ -1,12 +1,30 @@
 package com.eugeniojava.springdata;
 
+import com.eugeniojava.springdata.orm.Role;
+import com.eugeniojava.springdata.repository.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringDataApplication {
+public class SpringDataApplication implements CommandLineRunner {
+
+    private final RoleRepository roleRepository;
+
+    public SpringDataApplication(RoleRepository roleRepository) {
+        this.roleRepository = roleRepository;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringDataApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Role role = new Role();
+
+        role.setDescription("Software developer");
+
+        roleRepository.save(role);
     }
 }

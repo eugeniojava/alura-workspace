f506089564994b7f5b226c25eaf9e852a80c49ab
Eugênio Moreira <eugeniojava@outlook.com>
2021-07-06 13:10:47 -0300

    feat(android): apply layout to activity

--- android-parte-1-crie-a-sua-primeira-app-mobile/Agenda/app/src/main/java/com/eugeniojava/agenda/MainActivity.java
+++ android-parte-1-crie-a-sua-primeira-app-mobile/Agenda/app/src/main/java/com/eugeniojava/agenda/MainActivity.java
@@ -2,15 +2,28 @@ package com.eugeniojava.agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 
-public class MainActivity extends Activity {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class
+MainActivity extends Activity {
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Toast.makeText(this, "Eugênio Moreira", Toast.LENGTH_LONG).show();
+        setContentView(R.layout.activity_main);
+
+        List<String> alunos = new ArrayList<>(
+                Arrays.asList("Eugênio", "Alex", "Fran", "Maria", "Ana"));
+        ListView listaDeAlunos = findViewById(R.id.activity_main_lista_de_alunos);
+
+        listaDeAlunos.setAdapter(new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, alunos));
     }
 }
